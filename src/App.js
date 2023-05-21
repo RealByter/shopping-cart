160e7883221d275331032321462a2f88c118c101
Avishay Kuperman-Koren <justavishay@gmail.com>
2023-05-21 08:42:30 +0300

    feat: reset the default css

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import RouteSwitch from "./RouteSwitch";
+import "./normalize.css";
+import './reset.css'
+
+const App = () => {
+  return <RouteSwitch />
+}
+
+export default App;
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Shop from './components/pages/Shop';
 import Cart from './components/pages/Cart';
+import NavBar from './components/ui/NavBar';
 
 const RouteSwitch = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/shop" element={<Shop />} />
-				<Route path="/cart" element={<Cart />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+      <NavBar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/cart" element={<Cart />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	);
 };
 
