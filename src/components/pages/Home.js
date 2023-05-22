a5cf8f20aaeaa92925ee0e2a6f83cd4d8f8dec79
RealByter <justavishay@gmail.com>
2023-05-22 16:44:33 +0300

    feat: style the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,14 @@
+import './Home.css'
+import { NavLink } from 'react-router-dom';
+
 const Home = () => {
-	return <h1>Home</h1>;
+	return (
+		<div className="home">
+			<h2>Welcome to Fashion Factory</h2>
+			<p>We'll help you catch up with today's fashion trends!</p>
+			<NavLink to='/shop'>START SHOPPING!</NavLink>
+		</div>
+	);
 };
 
 export default Home;
