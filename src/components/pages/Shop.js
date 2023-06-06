a27edb025ca1c9b355cc74b1c96fa88dd4d8ccfc
Avishay Kuperman-Koren <justavishay@gmail.com>
2023-06-06 14:12:55 +0300

    feat: make the skeleton for the Shop page

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,5 +1,17 @@
-const Shop = () => {
-	return <h1>Shop</h1>;
+import ShopItem from '../items/ShopItem';
+import styles from './Shop.module.css';
+
+const Shop = props => {
+	return (
+		<div>
+			<h1 className={styles.header}>Shop</h1>
+			<div className={styles.shopItems}>
+				{props.items?.map((item, index) => (
+					<ShopItem key={index} {...item} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Shop;
