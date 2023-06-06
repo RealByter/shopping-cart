69a297dc0ee56450b2d06e3160539de7bbd7c784
Avishay Kuperman-Koren <justavishay@gmail.com>
2023-06-06 14:02:44 +0300

    refactor: remove RouteSwitch and move its content into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import RouteSwitch from './RouteSwitch';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './normalize.css';
 import './reset.css';
 import './App.css';
+import Home from './components/pages/Home';
+import Shop from './components/pages/Shop';
+import Cart from './components/pages/Cart';
+import NavBar from './components/ui/NavBar';
 
 const App = () => {
-	return <RouteSwitch />;
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/shop" element={<Shop />} />
+				<Route path="/cart" element={<Cart />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
